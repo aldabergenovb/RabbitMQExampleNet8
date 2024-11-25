77b5d38937a3c715b171ca09106db28e2b648f4d
Balabay Aldabergenov <baldabergenov@devexpert.kz>
2024-11-25 15:57:55 +0500

    initial commit

---
+++ RabbitMqConsumer/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+
+namespace RabbitMqConsumer;
+
+abstract class Program
+{
+    public static async Task Main(string[] args)
+    {
+        var factory = new ConnectionFactory() { HostName = "localhost" };
+
+        await using var connection = await factory.CreateConnectionAsync();
+        await using var channel = await connection.CreateChannelAsync();
+
+        await channel.QueueDeclareAsync(
+            queue: "hello",
+            durable: false,
+            exclusive: false,
+            autoDelete: false,
+            arguments: null
+        );
+
+        var consumer = new AsyncEventingBasicConsumer(channel);
+        consumer.ReceivedAsync += (_, es) =>
+        {
+            var body = es.Body.ToArray();
+            var message = Encoding.UTF8.GetString(body);
+            Console.WriteLine("Ok, received: {0}", message);
+            return Task.CompletedTask;
+        };
+
+        await channel.BasicConsumeAsync(
+            queue: "hello",
+            autoAck: true,
+            consumer: consumer
+        );
+        
+        Console.WriteLine("Press [enter] to exit");
+        while (Console.ReadLine() != "exit")
+        {
+            Thread.Sleep(1000);
+        }
+        
+    }
+}
---
+++ RabitMqPublisher/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using RabbitMQ.Client;
+
+namespace RabbitMqPublisher;
+
+abstract class Program
+{
+    public static async Task Main(string[] args)
+    {
+        var factory = new ConnectionFactory() { HostName = "localhost" };
+
+        await using var connection = await factory.CreateConnectionAsync();
+        await using var channel = await connection.CreateChannelAsync();
+
+        await channel.QueueDeclareAsync(
+            queue: "hello",
+            durable: false,
+            exclusive: false,
+            autoDelete: false,
+            arguments: null
+        );
+
+        Console.WriteLine("Publisher запущен. Введите текст для отправки сообщений. Для выхода введите `exit`.");
+
+        while (true)
+        {
+            Console.Write("Введите сообщение: ");
+            var message = Console.ReadLine();
+
+            if (message?.ToLower() == "exit")
+            {
+                Console.WriteLine("Завершение работы Publisher.");
+                break;
+            }
+
+            if (message != null)
+            {
+                var body = Encoding.UTF8.GetBytes(message);
+            
+                var basicProperties = new BasicProperties();
+
+                await channel.BasicPublishAsync(
+                    exchange: "",
+                    routingKey: "hello",
+                    mandatory: false,
+                    basicProperties: basicProperties,
+                    body: body,
+                    cancellationToken: CancellationToken.None);
+            }
+
+            Console.WriteLine($"Отправлено: {message}");
+        }
+    }
+}
